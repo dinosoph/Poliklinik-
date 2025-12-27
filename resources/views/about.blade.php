2c415c6fe9ddf3b3d81a3d7649ce67eb3cb94669
khairynakhairy <khairynakhairy@gmail.com>
2025-12-28 00:20:38 +0800

    Add about.blade.php and other changes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>About Us - MediTrust</title>
+    <link href="assets/css/main.css" rel="stylesheet">
+    <!-- Add other necessary meta tags and links to CSS -->
+</head>
+
+<body>
+    <!-- Include your header -->
+    <?php include 'header.php'; ?> <!-- This will link to your header file -->
+
+    <!-- About Section -->
+    <section id="about" class="about section">
+        <div class="container" data-aos="fade-up">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h3>Committed to Excellence in Healthcare</h3>
+                    <p>We have been serving our community with top-tier healthcare services for over 25 years. Our mission is to provide compassionate and comprehensive care to all patients.</p>
+                </div>
+                <div class="col-lg-6">
+                    <img src="assets/img/about.jpg" alt="Healthcare Commitment" class="img-fluid rounded-3">
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-lg-4">
+                    <div class="feature-item">
+                        <h4>25+ Years of Experience</h4>
+                        <p>Providing exceptional care and treatment for over two decades.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="feature-item">
+                        <h4>50,000+ Patients Treated</h4>
+                        <p>We are proud to have helped tens of thousands of patients.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="feature-item">
+                        <h4>Accredited Care</h4>
+                        <p>Our hospital is accredited by leading healthcare organizations.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="cta-group mt-4">
+                <a href="doctors.html" class="btn btn-primary">Meet Our Doctors</a>
+                <a href="services.html" class="btn btn-outline">View Our Services</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Include footer -->
+    <?php include 'footer.php'; ?> <!-- This will link to your footer file -->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('mainpage');
 });
+
+Route::get('/about', function () {
+    return view('about');  // About page
+})->name('about');
